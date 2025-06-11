8e2fa9752323be8613861df9eeace2c8993b142e
aloc23 <alannoelc@gmail.com>
2025-06-11 18:12:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-
 const fileMap = {
-  "quotes": [
-    "Fw_ DOLAN PCI PADEL COURTS.eml",
-    "004 Inxtremis 30 x 62 x 6m or 8m Sports Hall.pdf"
-  ],
-  "drawings": [
-    "club overview.pdf", "top view.pdf", "HG_HALL_GABLES 2.pdf",
-    "height.pdf", "HG_HALL_PLAN 2.pdf", "HG_HALL_LONGSIDES 2.pdf", "club.pdf"
-  ],
-  "folios": [
-    "philllips land folio.pdf", "DAFC folio_MH11730_041846.pdf"
-  ],
-  "renders": [
-    "x2.png", "aq.png", "Screenshot 2025-05-06 at 20.06.07.png", "x copy.png",
-    "1af.png", "x3.png", "3af.png", "z9.png", "6f.png", "z8.png", "4fa.png", "x4.png",
-    "z.png", "n8.png", "n.png", "bbb.png", "x.png", "a4.png", "aza.png", "az7.png",
-    "1b.png", "az6.png", "dddd.png", "aaaa.png", "az5.png", "ccc.png", "az1.png",
-    "ddd.png", "n10.png", "az3.png", "aaa.png", "2d.png", "d5.png", "x3 copy.png",
-    "x2 copy.png", "d4.png", "cccc.png", "Screenshot 2025-05-06 at 20.05.04.png",
-    "z10.png", "x12.png", "www.png", "b4.png", "n7.png", "z2.png", "z11.png", "w7.png",
-    "Screenshot 2025-05-06 at 20.06.19.png", "w.png", "az.png", "w6.png", "ww.png",
-    "w2.png", "z5.png", "z4.png", "w3.png", "z6.png", "c4.png", "a1b.png", "z7.png"
-  ],
-  "draft_contracts": [
+  "draft contracts": [
     "Mayweather_DAFC_Lease_Agreement_2025.pdf",
     "Mayweather_DAFC_Heads_of_Terms_2025.pdf"
   ],
-  "contracts": []
-};
+  "drawings": [
+    "club overview.pdf",
+    "HG_HALL_GABLES 2.pdf"
+  ],
+  "folios": [
+    "philllips land folio.pdf",
+    "DAFC folio_MH11730_041846.pdf"
+  ],
+  "quotes": [
+    "004 Inxtremis 30 x 62 x 6m or 8m Sports Hall.pdf",
+    "Fw_ DOLAN PCI PADEL COURTS.eml"
+  ],
+  "renders": [
+    "x2.png", "aq.png", "Screenshot 2025-05-06 at 20.06.07.png", "x copy.png", "1af.png", "x3.png",
+    "3af.png", "z9.png", "6f.png", "z8.png", "4fa.png", "x4.png", "z.png", "n8.png", "n.png",
+    "bbb.png", "x.png", "a4.png", "aza.png", "az7.png", "1b.png", "az6.png", "dddd.png",
+    "aaaa.png", "az5.png", "ccc.png", "az1.png", "ddd.png", "n10.png", "az3.png", "aaa.png",
+    "2d.png", "d5.png", "x3 copy.png", "x2 copy.png", "d4.png", "cccc.png",
+    "Screenshot 2025-05-06 at 20.05.04.png", "z10.png", "x12.png", "www.png", "b4.png",
+    "n7.png", "z2.png", "z11.png", "w7.png", "Screenshot 2025-05-06 at 20.06.19.png", "w.png",
+    "az.png", "w6.png", "ww.png", "w2.png", "z5.png", "z4.png", "w3.png", "z6.png", "c4.png",
+    "a1b.png", "z7.png"
+  ]
 };
 
-function loadFiles() {
-  const folder = document.getElementById("fileFolderSelect").value;
+function loadFileList() {
+  const folder = document.getElementById("folderSelect").value;
   const list = document.getElementById("fileList");
   const viewer = document.getElementById("fileViewer");
   list.innerHTML = "";
-  viewer.hidden = true;
+  viewer.src = "";
+
   if (!folder || !fileMap[folder]) return;
 
-  fileMap[folder].forEach(filename => {
-    const url = `files/${folder}/${filename}`;
+  fileMap[folder].forEach(file => {
     const li = document.createElement("li");
-    li.innerHTML = `<a href="#" onclick="viewFile('${url}'); return false;">${filename}</a>`;
+    const link = document.createElement("a");
+    link.href = `files/${folder}/${file}`;
+    link.textContent = file;
+    link.onclick = e => {
+      e.preventDefault();
+      viewer.src = `files/${folder}/${file}`;
+    };
+    li.appendChild(link);
     list.appendChild(li);
   });
-}
-
-function viewFile(url) {
-  const viewer = document.getElementById("fileViewer");
-  viewer.src = url;
-  viewer.hidden = false;
 }
